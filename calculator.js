0cde8e0ffa7cd6f191d24e42589a85888a3893aa
ztrottie <zakytrottier@hotmail.fr>
2024-11-20 22:00:06 -0500

    almost done with the logicc i am trying to make it work like the apple calculator just missing something , wont work for now

--- calculator.js
+++ calculator.js
@@ -14,32 +14,68 @@ function divide(nb1, nb2) {
 	return nb1 / nb2;
 }
 
-function operate(operator, nb1, nb2) {
-	let result;
-	if (operator === "/") {
-		result = divide(nb1, nb2);
-	} else if (operator === "*") {
-		result = multiply(nb1, nb2);
-	} else if (operator === "+") {
-		result = add(nb1, nb2)
-	} else if (operator === "-") {
-		result = subtract(nb1, nb2);
-	}
+function clear() {
+	input.value = "";
+	storedNumber = undefined;
+	operator = undefined;
 }
 
-let nb1;
-let nb2;
-let operator;
-const keypad = document.querySelector(".keypad");
 const input = document.querySelector("#input");
 
+function operate(operator, nb1, nb2) {
+	let result;
+	if (operator === "÷") {
+		result = divide(Number(nb1), Number(nb2));
+	} else if (operator === "*") {
+		result = multiply(Number(nb1), Number(nb2));
+	} else if (operator === "+") {
+		result = add(Number(nb1), Number(nb2))
+	} else if (operator === "-") {
+		result = subtract(Number(nb1), Number(nb2));
+	}
+	return result;
+}
+
+const keypad = document.querySelector(".keypad");
+let operator;
+let storedNumber;
+let resetKeypad = false
+
 keypad.addEventListener('click', function(event) {
-	let target = event.target;
-	if (target.tagName === "BUTTON") {
-		if (target.innerText === ".") {
+	if (event.target !== event.currentTarget) {
+		if (resetKeypad) {
+			resetKeypad = false;
+			input.value = '';
+		}
+		if (event.target.innerText === ".") {
 			if (input.value.lastIndexOf(".") !== -1)
 				return;
 		}
 		input.value += event.target.innerText;
 	}
 });
+
+const operators = document.querySelector(".operator");
+
+operators.addEventListener('click', function(event) {
+	if (event.target !== event.currentTarget) {
+		if (event.target.innerText === "clear") {
+			clear();
+			return;
+		}
+		if (!operator && input.value !== '') {
+			storedNumber = input.value;
+			operator = event.target.innerText;
+			console.log(`operator just pressed ${operator} stored number ${storedNumber}`);
+			resetKeypad = true;
+		} else if (operator && storedNumber && !resetKeypad) {
+			console.log(`preparing to do operation ${storedNumber} ${operator} ${input.value}`)
+			let result = operate(operator, storedNumber, input.value);
+			console.log(`operation done result is ${result}`);
+			input.value = result;
+			operator = undefined;
+			storedNumber = undefined;
+			resetKeypad = true;
+		}
+	}
+})
