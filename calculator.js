442a673f488301cd4e4b6c26a7e252dda33372a5
ztrottie <zakytrottier@hotmail.fr>
2024-12-08 10:58:05 -0500

    calculator done

--- calculator.js
+++ calculator.js
@@ -1,3 +1,5 @@
+let DEBUG = true;
+
 function add(nb1, nb2) {
 	return nb1 + nb2;
 }
@@ -15,16 +17,22 @@ function divide(nb1, nb2) {
 }
 
 function clear() {
+	if (DEBUG)
+		console.log("clearing calculator");
 	input.value = "";
 	storedNumber = undefined;
-	operator = undefined;
+	selectedOperator = undefined;
 }
 
 const input = document.querySelector("#input");
 
 function operate(operator, nb1, nb2) {
 	let result;
+	if (!operator && !nb2 && nb1)
+		return undefined;
 	if (operator === "÷") {
+		if (nb2 == 0)
+			return "Error";
 		result = divide(Number(nb1), Number(nb2));
 	} else if (operator === "*") {
 		result = multiply(Number(nb1), Number(nb2));
@@ -37,7 +45,7 @@ function operate(operator, nb1, nb2) {
 }
 
 const keypad = document.querySelector(".keypad");
-let operator;
+let selectedOperator;
 let storedNumber;
 let resetKeypad = false
 
@@ -59,23 +67,37 @@ const operators = document.querySelector(".operator");
 
 operators.addEventListener('click', function(event) {
 	if (event.target !== event.currentTarget) {
-		if (event.target.innerText === "clear") {
+		if (DEBUG) { 
+			console.log(
+			`
+				selected operator = '${selectedOperator}'
+				stored number = '${storedNumber}'
+				input value = '${input.value}'
+			`
+			)
+		}
+		let operator = event.target.innerText;
+		if (operator === "clear") {
 			clear();
 			return;
-		}
-		if (!operator && input.value !== '') {
-			storedNumber = input.value;
-			operator = event.target.innerText;
-			console.log(`operator just pressed ${operator} stored number ${storedNumber}`);
-			resetKeypad = true;
-		} else if (operator && storedNumber && !resetKeypad) {
-			console.log(`preparing to do operation ${storedNumber} ${operator} ${input.value}`)
-			let result = operate(operator, storedNumber, input.value);
-			console.log(`operation done result is ${result}`);
+		} else if (((selectedOperator && storedNumber) || operator === '=') && resetKeypad === false) {
+			if (DEBUG)
+				console.log("lets operate")
+			let result = operate(selectedOperator, storedNumber, input.value);
+			if (DEBUG)
+				console.log(`result: '${result}'`);
+			if (!result)
+				return;
+			if (result === 'Error')
+				resetKeypad = true;
 			input.value = result;
-			operator = undefined;
 			storedNumber = undefined;
+			selectedOperator = undefined
+		}
+		if (operator !== '=' && operator !== 'clear' && input.value !== 'Error') {
+			selectedOperator = operator;
 			resetKeypad = true;
+			storedNumber = input.value;
 		}
 	}
 })
